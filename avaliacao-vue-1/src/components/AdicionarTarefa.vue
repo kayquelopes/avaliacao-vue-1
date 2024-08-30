587751e553be471cd6447141cf6a2d347260a48c
kayquelopes <kayquelopes353@gmail.com>
2024-08-30 16:58:22 -0300

    first commit

---
+++ avaliacao-vue-1/src/components/AdicionarTarefa.vue
@@ -0,0 +1,24 @@
+<script setup>
+const model = defineModel()
+</script>
+
+<template>
+    <section>
+        <h2>Adicione uma nova tarefa</h2>
+        <input type="text" v-model="model" @keyup.enter="$emit('adicionarTarefa')" />
+        <button @click="$emit('adicionarTarefa')">Adicionar</button>
+    </section>
+</template>
+
+<style scoped>
+input {
+    width: 50%;
+    padding: 5px;
+    margin: 5px 0;
+}
+
+button {
+    padding: 5px;
+    margin: 5px 0;
+}
+</style>
